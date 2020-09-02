c392e36a1f6a8504446bab086055749065cf41ff
drunkenmonk <aoitori_@naver.com>
2020-09-02 12:34:07 +0000

    make some export file function for write a file containing jobs

--- python_files/modules/exporter.py
+++ python_files/modules/exporter.py
@@ -2,31 +2,50 @@ import os
 import csv
 import datetime
 import pytz
+import shutil
 
-def export_to_zip():
-  pass
+DIR_OUTPUT = "outputs"
+
+def export_to_zip(jobs):
+  dir_name = f"{DIR_OUTPUT}/{create_output_name()}"
+  if create_dir(dir_name):
+    dict_csv = {}
+    for job in jobs:
+      source_site = job.get("site")
+      print(source_site)
+      if source_site not in dict_csv:
+        dict_csv[source_site]["file"] = open(f"{dir_name}/{source_site}.csv", mode="w")
+        dict_csv[source_site]["writer"] = csv.writer(dict_csv[source_site]["file"])
+        dict_csv[source_site]["writer"].writerow(["site", "title", "company", "location", "salary", "link"])
+      dict_csv[source_site]["writer"].writerow(list(job.values()))
+    file = shutil.make_archive(dir_name, 'zip', dir_name)
+
+    return file
+
 
 def save_to_csv(jobs):
-  file = open("jobs.csv", mode="w")
-  writer = csv.writer(file)
-  print("before write")
-  writer.writerow(["site", "title", "company", "location", "salary", "link"])
-  print("after write")
-  for job in jobs:
-    print(job)
-    writer.writerow(list(job.values()))
-  return
+  if create_dir(DIR_OUTPUT):
+    dir_name = create_output_name()
+    file = open(f"{DIR_OUTPUT}/{dir_name}.csv", mode="w")
+    writer = csv.writer(file)
+    writer.writerow(["site", "title", "company", "location", "salary", "link"])
+    for job in jobs:
+      print(job)
+      writer.writerow(list(job.values()))
+    return file
 
 
 def create_dir(dir_name):
   try:
     if not os.path.exists(dir_name):
       os.mkdir(dir_name)
+    return True
   except OSError:
     print(f"{dir_name} is already exist")
+    return False
 
 
-def create_dir_name():
+def create_output_name():
   PREFIX = "jobs_at_"
   now = datetime.datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y%m%d_%H%M%S")[2:]
 
